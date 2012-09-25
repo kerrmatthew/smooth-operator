ac1e67a310f120d18ba070db83e5b82f57005285
matthew.kerr@me.com <matthew.kerr@me.com>
2012-09-25 18:00:42 +1200

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmoothOperator::Application.config.secret_token = '2bc7bd918615bcb40e6f9f428983ef0ce63b8ace21f23333a706b2a1c514eb401c213debdf04e714f3a857dc3fd4a8319a572ef52744aaa74c68063d1e9f5be2'
