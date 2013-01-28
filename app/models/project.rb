f2c448daac9f3f942823a6e28f0f2a10645eb8f1
matthew.kerr@me.com <matthew.kerr@me.com>
2013-01-29 10:11:13 +1300

    Polymorphic association reciprocated properly.

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
   attr_accessible :due_date, :name, :start_date, :status
 
   has_one :owner, class_name: :user
-  has_one :progress_item_delegate
+  has_one :progress_item_delegate, as: :progress_item, touch: true
   
   has_many :tasks
   has_many :members, through: :project_memberships, class_name: :user
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,8 @@
 class Task < ActiveRecord::Base
   attr_accessible :due_date, :name, :start_date, :status
 
+  has_one :progress_item_delegate, as: :progress_item, touch: true
+
   belongs_to :project
   belongs_to :assignee, class_name: :user
   belongs_to :assigner, class_name: :user
